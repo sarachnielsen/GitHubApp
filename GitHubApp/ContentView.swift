e3929c5189d124e65a8da5eaa630d52016191f56
Sara Nielsen <sara.c.h.nielsen@gmail.com>
2023-06-12 08:23:32 -0700

    Initial Commit

---
+++ GitHubApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHubApp
+//
+//  Created by Sara Nielsen on 6/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHubApp/GitHubAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubAppApp.swift
+//  GitHubApp
+//
+//  Created by Sara Nielsen on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
